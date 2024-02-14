1874277740de355a2b4c5b5aafdf766dbf04837c
Mohamed Samy <mohamedsamy0333@gmail.com>
2024-02-14 16:38:58 +0200

    14 feb edit

--- MCAL/MFMI/MFMI_interface.h
+++ MCAL/MFMI/MFMI_interface.h
@@ -16,19 +16,27 @@
 
 /*******************************************************************************************************/
 /*                                      Macros Of Sectors                                              */
-#define Flash_Sector_0	0b0000
-#define Flash_Sector_1	0b0001
-#define Flash_Sector_2	0b0010
-#define Flash_Sector_3	0b0011
-#define Flash_Sector_4	0b0100
-#define Flash_Sector_5	0b0101
+
+#define SECTOR_0	0
+#define SECTOR_1	1
+#define SECTOR_2	2
+#define SECTOR_3	3
+#define SECTOR_4	4
+#define SECTOR_5	5
+
+#define SECTOR_0_BASE_ADD 	0x08000000
+#define SECTOR_1_BASE_ADD 	0x08004000
+#define SECTOR_2_BASE_ADD 	0x08008000
+#define SECTOR_3_BASE_ADD 	0x0800C000
+#define SECTOR_4_BASE_ADD 	0x08010000
+#define SECTOR_5_BASE_ADD 	0x08020000
 
 /*******************************************************************************************************/
 /*                                      Function Prototypes                                            */
 /*******************************************************************************************************/
 void MFMI_voidEraseAppArea(void);
-void MFMI_voidSectorErase(u8 A_u8SectorNo);
-void MFMI_voidFlashWrite(u32 A_u32Address, u16* A_pu16Data, u16  A_u16Length);
+void MFMI_voidSectorErase (u8 A_u8SectorNo);
+void MFMI_voidFlashWrite  (u32 A_u32Address, u16* A_pu16Data, u16  A_u16Length);
 
 /*******************************************************************************************************/
 #endif /* MFMI_interface_H_ */
