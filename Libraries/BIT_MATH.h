79cfba049b095a6c42966e94ad6e314ed8ca977d
Rashad <Rashadelgarf3@gmail.com>
2024-04-06 14:58:50 +0200

    Adding SET_Val

--- Libraries/BIT_MATH.h
+++ Libraries/BIT_MATH.h
@@ -6,6 +6,7 @@
 #define CLR_BIT(REG,BIT_POSITION)   	REG &=~(1<<BIT_POSITION)
 #define TOG_BIT(REG,BIT_POSITION)  		REG ^= (1<<BIT_POSITION)
 #define GET_BIT(REG,BIT_POSITION)     	(REG & (1<<BIT_POSITION))>>BIT_POSITION
+#define SET_VAL(Var,Bit_No,Val)		    (Var |= (Val<<Bit_No))
 //Register operation (with mask)
 #define MSK_BITS(REG,MSK)				(REG) |=  (MSK)
 #define Clr_BITS(REG,MSK)				(REG) &= ~(MSK)
